479949e1f0482b8777b6f5a9607aee399aed2355
Pranav Kapoor <pranavkapoor99@gmail.com>
2018-08-09 16:56:15 +0530

    Update electronics.js

--- routes/electronicsData/electronics.js
+++ routes/electronicsData/electronics.js
@@ -1,7 +1,7 @@
 var router=require('express').Router();
 var electronicsData=require('../../model/electronicsModel');
 var portDev= require('../../../config');
-const port=process.env.PORT ||portDev.port;
+const port=process.env.PORT ||configFile.port || 1234;
 
 
 router.get('/:pageNumber',function (req,res) {
